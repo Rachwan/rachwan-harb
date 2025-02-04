d01e68583da56b542620097d6bd891ba3cf5a86f
Rachwan <rachwan.harb2023@gmail.com>
2025-02-04 17:05:26 +0200

    Updated the Home page - Full Stack

--- components/DownloadButton.jsx
+++ components/DownloadButton.jsx
@@ -14,14 +14,12 @@ const downloadFileAtURL = (url) => {
 };
 
 const DownloadButton = () => {
-  const FRONT_END_PDF_FILE_URL = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/assets/CVs/Rachwan_Harb_Front-End_Developer.pdf`;
-  const FULL_STACK_PDF_FILE_URL = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/assets/CVs/Rachwan_Harb_Full-Stack_Developer.pdf`;
-
-  const UPDATED_FRONT_END_PDF_FILE_URL = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/assets/CVs/Rachwan_Harb_Front-End_Engineer.pdf`;
+  // const FRONT_END_PDF_FILE_URL = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/assets/CVs/Rachwan_Harb_Front-End_Developer.pdf`;
+  const FULL_STACK_PDF_FILE_URL = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/assets/CVs/Rachwan Harb – Full-Stack Web Developer.pdf`;
 
   const handleDownload = (event) => {
     event.preventDefault();
-    downloadFileAtURL(UPDATED_FRONT_END_PDF_FILE_URL);
+    downloadFileAtURL(FULL_STACK_PDF_FILE_URL);
   };
 
   return (
